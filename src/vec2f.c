2aec0844e0ade1d9e943cb50a7e7530221ae0dab
Eemil Majuri <eemilmajuri@gmail.com>
2023-07-11 19:51:26 +0300

    Created vector functions

--- src/vec2f.c
+++ src/vec2f.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vec2f.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 19:05:36 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/07/11 19:06:38 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/07/11 19:50:12 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,32 @@ t_vec2f	vec2f_add(t_vec2f lhs, t_vec2f rhs)
 
 	out.x = lhs.x + rhs.x;
 	out.y = lhs.y + rhs.y;
+	return (out);
+}
+
+t_vec2f	vec2f_sub(t_vec2f lhs, t_vec2f rhs)
+{
+	t_vec2f	out;
+
+	out.x = lhs.x - rhs.x;
+	out.y = lhs.y - rhs.y;
+	return (out);
+}
+
+t_vec2f	vec2f_mul(t_vec2f lhs, t_vec2f rhs)
+{
+	t_vec2f	out;
+
+	out.x = lhs.x * rhs.x;
+	out.y = lhs.y * rhs.y;
+	return (out);
+}
+
+t_vec2f	vec2f_div(t_vec2f lhs, t_vec2f rhs)
+{
+	t_vec2f	out;
+
+	out.x = lhs.x / rhs.x;
+	out.y = lhs.y / rhs.y;
 	return (out);
 }
