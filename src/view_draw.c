1aa121c31d66d688ac37367fbe9c919a619492f5
Tuomas <tuomas.jaasalo@alceon.fi>
2023-07-11 17:07:12 +0300

    Added placeholder fn for view_draw

--- src/view_draw.c
+++ src/view_draw.c
@@ -6,9 +6,16 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 16:51:03 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/07/11 16:51:18 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/07/11 17:06:17 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "view.h"
 
+void	view_draw(t_view *self, t_scene *scene)
+{
+	(void)scene;
+	// Draw full background or clear view->back
+	// Draw world
+	view_swap_buffers(self);
+}
