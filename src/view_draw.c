806af5608aef591fd85ae117cd2b8f694c99194a
Tuomas <tuomas.jaasalo@alceon.fi>
2023-07-11 18:52:31 +0300

    Actually put the image to window

--- src/view_draw.c
+++ src/view_draw.c
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   view_draw.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 16:51:03 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/07/11 17:13:13 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/07/11 18:52:18 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <mlx.h>
 #include "view.h"
 
 void	draw_background(t_image *image, t_scene *scene)
@@ -33,7 +34,12 @@ void	draw_background(t_image *image, t_scene *scene)
 
 void	view_draw(t_view *self, t_scene *scene)
 {
-	draw_background(self->front, scene);
+	draw_background(self->back, scene);
 	// Draw world
 	view_swap_buffers(self);
+	mlx_put_image_to_window(
+		self->window->mlx,
+		self->window->window,
+		self->front->img,
+		0, 0);
 }
