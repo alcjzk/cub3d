a3cc0be9ee5d0a9ed634c37461ca54689a113a38
Tuomas <tuomas.jaasalo@alceon.fi>
2023-07-11 21:11:30 +0300

    Removed unnecessary temp value

--- src/vec2f.c
+++ src/vec2f.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vec2f.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 19:05:36 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/07/11 19:50:12 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/07/11 21:10:58 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,28 @@
 
 t_vec2f	vec2f_add(t_vec2f lhs, t_vec2f rhs)
 {
-	t_vec2f	out;
-
-	out.x = lhs.x + rhs.x;
-	out.y = lhs.y + rhs.y;
-	return (out);
+	lhs.x += rhs.x;
+	lhs.y += rhs.y;
+	return (lhs);
 }
 
 t_vec2f	vec2f_sub(t_vec2f lhs, t_vec2f rhs)
 {
-	t_vec2f	out;
-
-	out.x = lhs.x - rhs.x;
-	out.y = lhs.y - rhs.y;
-	return (out);
+	lhs.x -= rhs.x;
+	lhs.y -= rhs.y;
+	return (lhs);
 }
 
 t_vec2f	vec2f_mul(t_vec2f lhs, t_vec2f rhs)
 {
-	t_vec2f	out;
-
-	out.x = lhs.x * rhs.x;
-	out.y = lhs.y * rhs.y;
-	return (out);
+	lhs.x *= rhs.x;
+	lhs.y *= rhs.y;
+	return (lhs);
 }
 
 t_vec2f	vec2f_div(t_vec2f lhs, t_vec2f rhs)
 {
-	t_vec2f	out;
-
-	out.x = lhs.x / rhs.x;
-	out.y = lhs.y / rhs.y;
-	return (out);
+	lhs.x /= rhs.x;
+	lhs.y /= rhs.y;
+	return (lhs);
 }
