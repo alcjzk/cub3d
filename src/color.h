f744b0acea5c0619bfc4a3f56c7f1f94b5673508
Tuomas Jääsalo <tuomas.jaasalo@alceon.fi>
2023-08-30 08:35:11 +0300

    Corrected color structure

--- src/color.h
+++ src/color.h
@@ -3,28 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   color.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 13:53:37 by emajuri           #+#    #+#             */
-/*   Updated: 2023/08/01 17:36:01 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/08/30 08:33:22 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COLOR_H
 # define COLOR_H
 
+# include <stdint.h>
+
 typedef struct s_color_rgba
 {
-	unsigned char	a;
-	unsigned char	b;
-	unsigned char	g;
 	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+	unsigned char	a;
 }	t_color_rgba;
 
 typedef union u_color
 {
-	int				value;
 	t_color_rgba	channels;
+	int32_t			i32;
+	uint32_t		u32;
 }	t_color;
 
 #endif
