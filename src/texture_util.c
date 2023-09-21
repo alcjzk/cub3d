1d675b4c081eec7d8c7fad141fb117d10aac24f3
Eemil Majuri <eemilmajuri@gmail.com>
2023-09-21 14:22:10 +0300

    Added div by 0 protecc

--- src/texture_util.c
+++ src/texture_util.c
@@ -6,7 +6,7 @@
 /*   By: tjaasalo <tjaasalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 10:36:08 by tjaasalo          #+#    #+#             */
-/*   Updated: 2023/09/21 10:36:49 by tjaasalo         ###   ########.fr       */
+/*   Updated: 2023/09/21 14:42:39 by tjaasalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 _Bool	texture_is_valid(mlx_texture_t *self)
 {
 	if (!self)
+		return (FALSE);
+	if (!self->height || !self->width)
 		return (FALSE);
 	if (self->width % self->height != 0)
 		return (FALSE);
